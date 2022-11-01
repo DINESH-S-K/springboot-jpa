88746741992a4df3c11cc70f59090c6cc7f084f6
Dinesh S K <dineshsrinivasan2001@gmail.com>
2022-11-01 15:38:00 +0530

    Delete Mapping API /manytomanymapping bidirectional testing directory

--- Mapping API /manytomanymapping bidirectional testing/src/main/java/com/c1x/manytomanymapping/repository/StudentsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.c1x.manytomanymapping.repository;
-
-import com.c1x.manytomanymapping.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentsRepository extends JpaRepository<Student,Long> {
-}
